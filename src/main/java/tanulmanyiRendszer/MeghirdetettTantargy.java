65ec79bfa77a6ca49de36948673f9b28b60d2c20
Kiss Sándor Ádám <kisssandoradam@gmail.com>
2014-05-23 01:54:05 +0200

    Added comments for MeghirdetettTantargy.java

--- src/main/java/tanulmanyiRendszer/MeghirdetettTantargy.java
+++ src/main/java/tanulmanyiRendszer/MeghirdetettTantargy.java
@@ -6,12 +6,41 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Tartalmazza a tantárgyat, előadót, félévet, előadás időpontját,
+ * előadás helyét (terem) és a gyakorlati csoportokat.
+ * @author adam
+ *
+ */
 public class MeghirdetettTantargy {
+	/**
+	 * A tantárgy, ami meg lett hirdetve.
+	 */
 	private Tantargy tantárgy;
+	
+	/**
+	 * A tantárgy előadója.
+	 */
 	private Oktato előadó;
+	
+	/**
+	 * Ebben a félévben lett meghirdetve a tantárgy.
+	 */
 	private Felev aktuálisFélév;
+	
+	/**
+	 * Az előadás időpontja.
+	 */
 	private Idopont előadásIdőpont;
+	
+	/**
+	 * Az előadás terme.
+	 */
 	private String előadásTerem;
+	
+	/**
+	 * A tantárgyhoz kapcsolódó gyakorlati csoportok listája.
+	 */
 	private List<GyakorlatiCsoport> gyakorlatiCsoportok;
 	
 	/**
@@ -19,6 +48,14 @@ public class MeghirdetettTantargy {
 	 */
 	private static Logger logger = LoggerFactory.getLogger(MeghirdetettTantargy.class);
 	
+	/**
+	 * 
+	 * @param tantárgy A meghirdetendő tantárgy.
+	 * @param előadó Az az {@link Oktato}, aki az előadást tartja.
+	 * @param aktuálisFélév	A félév, melyben meg lesz hirdetve a tantárgy.
+	 * @param előadásIdőpont Az előadás időpontja.
+	 * @param előadásTerem Az előadás ebben a teremben lesz megtarva.
+	 */
 	public MeghirdetettTantargy(Tantargy tantárgy, Oktato előadó,
 			Felev aktuálisFélév, Idopont előadásIdőpont, String előadásTerem) {
 		this.tantárgy = tantárgy;
@@ -30,30 +67,68 @@ public class MeghirdetettTantargy {
 		logger.trace("Új {} lett példányosítva: {}.", new Object[] {getClass().getName(), this});
 	}
 
+	/**
+	 * Visszaadja, hogy melyik {@link Tantargy} lett meghirdetve.
+	 * 
+	 * @return Visszaadja, hogy melyik {@link Tantargy} lett meghirdetve.
+	 */
 	public Tantargy getTantárgy() {
 		return tantárgy;
 	}
 
+	/**
+	 * Visszaadja azt az {@link Oktato}-t, aki az előadást tartja.
+	 * 
+	 * @return Visszaadja azt az {@link Oktato}-t, aki az előadást tartja.
+	 */
 	public Oktato getElőadó() {
 		return előadó;
 	}
 
+	/**
+	 * Visszaadja, hogy melyik félévben lett meghirdetve a tantárgy.
+	 * 
+	 * @return Visszaadja, hogy melyik félévben lett meghirdetve a tantárgy.
+	 */
 	public Felev getAktuálisFélév() {
 		return aktuálisFélév;
 	}
 
+	/**
+	 * Visszaadja az előadás időpontját.
+	 * 
+	 * @return Visszaadja az előadás időpontját.
+	 */
 	public Idopont getElőadásIdőpont() {
 		return előadásIdőpont;
 	}
 
+	/**
+	 * Visszaadja az előadás termét {@link String}-ként.
+	 * @return Visszaadja az előadás termét {@link String}-ként.
+	 */
 	public String getElőadásTerem() {
 		return előadásTerem;
 	}
 
+	/**
+	 * Visszaadja a tantárgyhoz tartozó gyakorlati csoportokat egy listában.
+	 * 
+	 * @return Visszaadja a tantárgyhoz tartozó gyakorlati csoportokat egy listában.
+	 */
 	public List<GyakorlatiCsoport> getGyakorlatiCsoportok() {
 		return gyakorlatiCsoportok;
 	}
 
+	/**
+	 * Az {@link #aktuálisFélév#hashCode()}-ja,
+	 * az {@link #előadásIdőpont#hashCode()}-ja,
+	 * az {@link #előadásTerem#hashCode()}-ja
+	 * és a {@link #tantárgy#hashCode()}-ja alapján
+	 * generálódik.
+	 * 
+	 * @return Visszaadja a {@link MeghirdetettTantargy} hashCode-ját.
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -70,6 +145,17 @@ public class MeghirdetettTantargy {
 		return result;
 	}
 
+	/**
+	 * Két {@link MeghirdetettTantargy} akkor tekinthető egyformának, ha
+	 * ugyan az a {@link Tantargy} lett meghirdetve,
+	 * ugyan abban a {@link Felev}-ben,
+	 * ugyan azok az előadás időpontjaik, termeik és
+	 * ugyan az az előadó tanár tartja.
+	 * 
+	 * @param obj Ezzel az objektummal lesz összehasonlítva az aktuális példány.
+	 * @return <code>true</code> ha a paraméterül kapott objektum megegyezik
+	 * az aktuális példánnyal, <code>false</code> egyébként.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -120,6 +206,11 @@ public class MeghirdetettTantargy {
 		return true;
 	}
 	
+	/**
+	 * Visszaadja az aktuális példány sztringreprezentációját.
+	 * 
+	 * @return Visszaadja az aktuális példány sztringreprezentációját.
+	 */
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
