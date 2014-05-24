cc34f57f8b9139b4eb320d29ca6c0908fde8f59c
Kiss Sándor Ádám <kisssandoradam@gmail.com>
2014-05-24 22:08:11 +0200

    getConnection() now uses classloader.

--- src/main/java/db/ConnectionHelper.java
+++ src/main/java/db/ConnectionHelper.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,21 +19,6 @@ public class ConnectionHelper {
 	private static final Logger logger = LoggerFactory.getLogger(ConnectionHelper.class);
 	
 	/**
-	 * Az adatbázishoz való kapcsolódás url-je.
-	 */
-	private static final String url = "jdbc:oracle:thin:@db.inf.unideb.hu:1521:ora11g";
-	
-	/**
-	 * Az adatbázishoz kapcsolódó felhasználó neve.
-	 */
-	private static final String dbFelhasználónév = "H_C2MQ5N";
-	
-	/**
-	 * Az adatbázishoz kapcsolódó felhasználó jelszava.
-	 */
-	private static final String dbJelszó = "progtech";
-	
-	/**
 	 * Az adatbázis kapcsolata.
 	 */
 	private static Connection conn;
@@ -40,34 +26,26 @@ public class ConnectionHelper {
 	/**
 	 * Csatlakozik a db.inf.unideb.hu adatbázishoz és visszaad egy ezt leíró {@link Connection} objektumot.
 	 * 
-	 * @param felhasználónév Az adatbázis kapcsolódó felhasználó felhasználóneve.
-	 * @param jelszó Az adatbázis kapcsolódó felhasználó jelszava.
 	 * @return Egy {@link Connection}-t ad vissza, ami a db.inf.unideb.hu-ra csatlakozik.
 	 * @throws IOException ha nem tud kapcsolódni az adatbázishoz.
 	 * @throws SQLException	ha egy adatbázisbeli hiba adódik.
 	 */
-	public static Connection getConnection(String felhasználónév, String jelszó)
-			throws IOException, SQLException {
+	public static Connection getConnection()  throws IOException, SQLException {
 		if (conn == null || conn.isClosed()) {
 			synchronized (ConnectionHelper.class) {
-				if (conn == null) {
-					conn = DriverManager.getConnection(url, felhasználónév, jelszó);
+				if (conn == null || conn.isClosed()) {
+					Properties dbProps = new Properties();
+					dbProps.load(dbProps.getClass().getResourceAsStream("/dbconnection.properties"));
+					
+					conn = DriverManager.getConnection(
+							dbProps.getProperty("url"),
+							dbProps.getProperty("username"),
+							dbProps.getProperty("password"));
 					logger.info("Új adatbázis kapcsolat jött létre.");
 				}
 			}
 		}
 		return conn;
-	}
-	
-	/**
-	 * Csatlakozik a db.inf.unideb.hu adatbázishoz és visszaad egy ezt leíró {@link Connection} objektumot.
-	 * 
-	 * @return Egy {@link Connection}-t ad vissza, ami a db.inf.unideb.hu-ra csatlakozik.
-	 * @throws IOException ha nem tud kapcsolódni az adatbázishoz.
-	 * @throws SQLException	ha egy adatbázisbeli hiba adódik.
-	 */
-	public static Connection getConnection() throws IOException, SQLException {
-		return getConnection(dbFelhasználónév, dbJelszó);
 	}
 	
 	/**
--- src/test/java/db/ConnectionTest.java
+++ src/test/java/db/ConnectionTest.java
@@ -16,20 +16,14 @@ public class ConnectionTest {
 		ConnectionHelper.closeConnection();
 	}
 	
-	@Test(expected = SQLException.class)
-	public void getConnectionTest1() throws IOException, SQLException {
-		ConnectionHelper.getConnection("noname","nopassword");
-		fail("El kellett volna buknia a tesztnek, mert nincs ilyen felhasználó/jelszó páros.");
-	}
-	
 	@Test
-	public void getConnectionTest2() throws IOException, SQLException {
+	public void getConnectionTest1() throws IOException, SQLException {
 		Connection conn = ConnectionHelper.getConnection();
 		assertNotNull(conn);
 	}
 	
 	@Test
-	public void getConnectionTest3() throws IOException, SQLException {
+	public void getConnectionTest2() throws IOException, SQLException {
 		Connection conn = ConnectionHelper.getConnection();
 		assertNotNull(conn);
 		conn = ConnectionHelper.getConnection();
@@ -37,7 +31,7 @@ public class ConnectionTest {
 	}
 	
 	@Test
-	public void getConnectionTest4() throws IOException, SQLException {
+	public void getConnectionTest3() throws IOException, SQLException {
 		Connection conn1 = ConnectionHelper.getConnection();
 		Connection conn2 = ConnectionHelper.getConnection();
 		assertSame(conn1, conn2);
