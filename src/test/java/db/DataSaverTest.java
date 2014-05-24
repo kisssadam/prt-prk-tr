cbb96492a75601aa327f0f0ef91a17411dec9bcd
Kiss Sándor Ádám <kisssandoradam@gmail.com>
2014-05-24 03:04:37 +0200

    Added DataSaverTest.java

--- src/test/java/db/DataSaverTest.java
+++ src/test/java/db/DataSaverTest.java
@@ -1,5 +1,20 @@
 package db;
 
-public class DataSaverTest {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class DataSaverTest {
+	
+	@AfterClass
+	public static void closeDBConnection() throws SQLException {
+		ConnectionHelper.closeConnection();
+	}
+	
+	@Test
+	public void saveTest() throws SQLException, IOException {
+		DataSaver.save();
+	}
 }
